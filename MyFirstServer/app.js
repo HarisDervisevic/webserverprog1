4b3330e2a273673db532dd9768a155528e9792ec
harisssssss <haris.dervisevic@elev.ga.ntig.se>
2020-09-03 13:17:47 +0200

    Första webbservern

--- MyFirstServer/app.js
+++ MyFirstServer/app.js
@@ -1,1 +1,11 @@
-hello niklas
+const express = require('express')
+const app = express()
+const port = 3000
+
+const clientDir = __dirname + "\\client\\"
+
+app.get('/', (req, res) => res.sendFile(__dirname + "\\client\\index.html"))
+app.get('/style', (req,res) =>{res.sendFile(clientDir + "style.css")})
+app.get('/picture', (req,res) =>{res.sendFile(clientDir + "messi.jpg")})
+
+app.listen(port, () => console.log('Example app listening on port ${port}!'))
