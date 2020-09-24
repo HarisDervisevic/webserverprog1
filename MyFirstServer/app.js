4f6d8b87a3ba2a587f0ac24d50994e77677e6b30
harisssssss <haris.dervisevic@elev.ga.ntig.se>
2020-09-24 13:11:23 +0200

    sparat input i databasen

--- MyFirstServer/app.js
+++ MyFirstServer/app.js
@@ -2,7 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+
 const clientDir = __dirname + "\\client\\"
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  email: String
+});
+
+const Person = mongoose.model('person', personSchema);
 
 app.use(express.json())
 app.use(express.urlencoded())
@@ -16,5 +26,7 @@ app.listen(port, () => console.log('Example app listening on port ${port}!'))
 app.post('/', (req, res) => {
     console.log(req.body.name)
     console.log(req.body.email)
+    const person = new Person({ name: req.body.name, email: req.body.email});
+    person.save();
   res.redirect('/')
 })
